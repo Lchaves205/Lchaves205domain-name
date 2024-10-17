2087a75fcd1db67df9c895e0d8b67a71158b33f6
Leo Chaves <leingas13@gmail.com>
2024-10-17 03:17:28 +0000

    updated JS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our", "his", "her", "their", "it"];
+  let adj = ["great", "big", "small", "fat", "pretty", "beautiful"];
+  let noun = ["jogger", "racoon", "cat", "king", "princess", "mermaid"];
+
+  pronoun.forEach(articulo => {
+    adj.forEach(adjetivo => {
+      noun.forEach(sujeto => {
+        console.log(articulo + adjetivo + sujeto + ".com");
+      });
+    });
+  });
   console.log("Hello Rigo from the console!");
 };
